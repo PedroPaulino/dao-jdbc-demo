f8ddca252d63e7229ad9280b8a8a6f9dd7b1d2d8
Pedro_paulino <pedropaulinoeng@hotmail.com>
2022-11-23 17:25:37 -0300

    findAll Implementation

--- src/application/program.java
+++ src/application/program.java
@@ -30,6 +30,13 @@ public class program {
 			System.out.println(obj);
 		}
 		
+		System.out.println("==== TEST 3: seller findAll ====");
+
+		list = sellerDao.findAll();
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
+		
 		
 		
 	}
